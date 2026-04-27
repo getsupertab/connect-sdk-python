b8a1c4eba90b65fbe6039102fe7bf2d132d0a041
Nikita Kovalev <nick.tommybee37@gmail.com>
2026-04-27 11:45:02 +0200

    Make Sec-Ch-UA header exceptions precise and up-to-date

--- connect/merchant/bots.py
+++ connect/merchant/bots.py
@@ -25,6 +25,36 @@ _KNOWN_BOT_UA_SUBSTRINGS = (
     "wget",
 )
 
+_CHROMIUM_UA_SUBSTRINGS = (
+    "chrome/",
+    "chromium/",
+    "edg/",
+    "edga/",
+    "opr/",
+    "opera/",
+    "samsungbrowser/",
+)
+_IOS_WEBKIT_BROWSER_UA_SUBSTRINGS = ("crios/", "edgios/", "fxios/")
+
+
+def _user_agent_can_omit_sec_ch_ua(lower_case_user_agent: str) -> bool:
+    """Return whether this UA is allowed to omit the Sec-CH-UA header.
+
+    Chromium-family browsers generally send Sec-CH-UA, but most browser
+    user-agent strings still include Mozilla and Safari compatibility tokens.
+    Keep this exception limited to browsers/platforms that do not reliably
+    support UA Client Hints: Safari, Firefox, and iOS WebKit browser wrappers.
+    """
+    is_firefox = "firefox/" in lower_case_user_agent or "fxios/" in lower_case_user_agent
+    is_ios_webkit_browser = any(browser in lower_case_user_agent for browser in _IOS_WEBKIT_BROWSER_UA_SUBSTRINGS)
+    is_safari = (
+        "safari/" in lower_case_user_agent
+        and "applewebkit/" in lower_case_user_agent
+        and not any(browser in lower_case_user_agent for browser in _CHROMIUM_UA_SUBSTRINGS)
+    )
+
+    return is_firefox or is_ios_webkit_browser or is_safari
+
 
 def default_bot_detector(request: Request) -> bool:
     user_agent = request.headers.get("user-agent", "")
@@ -41,7 +71,7 @@ def default_bot_detector(request: Request) -> bool:
     )
     missing_headers = not accept or not accept_language
 
-    if "safari" in lower_case_user_agent or "mozilla" in lower_case_user_agent:
+    if _user_agent_can_omit_sec_ch_ua(lower_case_user_agent):
         if headless_indicators and is_browser_missing_sec_ch_ua:
             return False
 
